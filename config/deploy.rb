c91094e976ce7444f19d7f205d13cc01071e9717
tsuny221 <kingoftsuny@gmail.com>
2020-07-08 08:01:42 +0000

    [Update] deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,8 @@
 lock "~> 3.14.1"
 
 set :application, "bookers_app" # my_app_name → アプリケーション名
-set :repo_url, "git@github.com:tsuny221/bookers_app.git" # git@example.com:me/my_repo.git → GitHubに登録しているアプリのアドレス
-# アプリのアドレスは$ git remote -v コマンドで確認できます
+set :repo_url, "https://github.com/tsuny221/bookers_app.git" # git@example.com:me/my_repo.git → GitHubに登録しているアプリのアドレス
+# アプリのアドレスは$ bundle exec cap installコマンドで確認できます
 set :deploy_to, "/home/ec2-user/bookers_app"
 set :rbenv_ruby, '2.5.7'
 set :linked_files, %w{config/master.key .env}
